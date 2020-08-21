6aaa81aba36764e96124bfbfee28d325f94aef2c
lujianxiang <409627448@qq.com>
2020-08-21 11:14:15 +0800

    second

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-// import vuedemoNpmPractice from "../index";
-// Vue.use(vuedemoNpmPractice);
-
-new Vue({
-  el: "#app",
-  render: h => h(App)
-});
